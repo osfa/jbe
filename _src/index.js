61cb04d27e9f086dc909a8d306dffea8d09c361d
jacob broms <jacob.broms@sanocore.se>
2020-11-25 16:45:56 +0100

    respon reel

--- _src/index.js
+++ _src/index.js
@@ -1,8 +1,24 @@
 // CSS and SASS files
 import './index.scss';
-// import './lazysizes.min.js';
+
 const el = document.querySelector('.video-container');
 
-el.onclick = function() {
-  el.classList.toggle('active');
+if(el){
+    el.onclick = function() {
+      el.classList.toggle('active');
+    }
 }
+
+const mainVideo = document.querySelector('#bgvid');
+const lowQ = 'reel-mobile.mp4'
+const hiQ = 'reel-desktop.mp4'
+const superQ = 'reel-max.mp4'
+
+if(mainVideo){
+    if (window.innerWidth < 1024) {
+      mainVideo.innerHTML = "<source type='video/mp4' src='" + lowQ + "' />";
+    } else {
+      mainVideo.innerHTML = "<source type='video/mp4' src='" + hiQ + "' />";
+    }
+}
+
